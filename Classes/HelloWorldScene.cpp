cf84b9fd01f36ed02707263d3c3e4e001eb9e74b
chenchen <chenchen201252@sogou-inc.com>
2014-05-09 14:39:25 +0800

    add direction touch and keyboard logic

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -2,11 +2,10 @@
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
-{
+Scene* HelloWorld::createScene() {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
+
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
@@ -18,74 +17,94 @@ Scene* HelloWorld::createScene()
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
-{
+bool HelloWorld::init() {
     //////////////////////////////
     // 1. super init first
-    if ( !Layer::init() )
-    {
+    if (!Layer::init()) {
         return false;
     }
-    
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Point origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    //CC: bind touch listener
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+    touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+    getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-	closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+    //CC: bind keyboard listener
+    auto keyboardListener = EventListenerKeyboard::create();
+    keyboardListener->onKeyPressed = CC_CALLBACK_2(HelloWorld::onKeyPressed, this);
+    getEventDispatcher()->addEventListenerWithSceneGraphPriority(keyboardListener, this);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Point::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = LabelTTF::create("Hello World", "Arial", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Point(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
     return true;
 }
 
+bool HelloWorld::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event) {
+    //CC: get beginning touch point
+    auto touchPoint = touch->getLocation();
+    beginX = touchPoint.x;
+    beginY = touchPoint.y;
+    return true;
+}
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
-{
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-    return;
-#endif
+void HelloWorld::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *unused_event) {
+    //CC: get move direction
+    auto touchPoint = touch->getLocation();
+    endX = beginX - touchPoint.x;
+    endY = beginY - touchPoint.y;
 
-    Director::getInstance()->end();
+    //CC: figure direction
+    if (abs(endX) > abs(endY)) {
+        if(endX > 0)
+            doLeft();
+        else
+            doRight();
+    } else {
+        if(endY > 0)
+            doDown();
+        else
+            doUp();
+    }
+}
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+void HelloWorld::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event) {
+    switch (keyCode) {
+        case EventKeyboard::KeyCode::KEY_LEFT_ARROW: {
+            doLeft();
+            break;
+        }
+        case EventKeyboard::KeyCode::KEY_RIGHT_ARROW: {
+            doRight();
+            break;
+        }
+        case EventKeyboard::KeyCode::KEY_UP_ARROW: {
+            doUp();
+            break;
+        }
+        case EventKeyboard::KeyCode::KEY_DOWN_ARROW: {
+            doDown();
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+bool HelloWorld::doLeft() {
+    log("touch left");
+    return true;
+}
+
+bool HelloWorld::doRight() {
+    log("touch right");
+    return true;
+}
+
+bool HelloWorld::doUp() {
+    log("touch up");
+    return true;
+}
+
+bool HelloWorld::doDown() {
+    log("touch down");
+    return true;
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -10,13 +10,25 @@ public:
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
+    virtual bool init();
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+    //CC: touch and keyboard event listener
+    virtual bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *unused_event);
+    virtual void onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *unused_event);
+    virtual void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+
+    //CC: move function
+    bool doUp();
+    bool doDown();
+    bool doLeft();
+    bool doRight();
+
+private:
+    //CC: touch point
+    int beginX, beginY, endX, endY;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
